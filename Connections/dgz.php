12cc108477f0eb1a9fce2b33fbad2998b8bbc2a1
pom <pom@creativechili.com>
2017-10-21 14:13:10 +0700

    Prepare for dev site

--- Connections/dgz.php
+++ Connections/dgz.php
@@ -2,13 +2,13 @@
 	date_default_timezone_set ("Asia/Bangkok");
 	
 	$hostname_dgz = "localhost";
-	$database_dgz = "site";
-	$username_dgz = "dgzoo";
-	$password_dgz = "dgzoo.thaweephan";
+	$database_dgz = "dev_site";
+	$username_dgz = "creativechili";
+	$password_dgz = "creativechili";
 	$dgz = mysql_connect($hostname_dgz, $username_dgz, $password_dgz) or trigger_error(mysql_error(),E_USER_ERROR); 
 	
 	mysql_query("SET NAMES 'utf8'");
 	mysql_select_db($database_dgz,$dgz);
 
-	$url = "http://www.thaweephan.co.th";
+	$url = "http://dev.thaweephanwoodservice.com/";
 ?>
